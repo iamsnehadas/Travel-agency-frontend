e06c557a51bec8de011c199026c41503971b2c96
iamsnehadas <snehasnow8@gmail.com>
2024-12-20 19:07:33 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_BASE_URL = 'https://travel-agency-backend-3yhw.onrender.com/';
+const API_BASE_URL = 'https://travel-agency-backend-3yhw.onrender.com';
 
 export default API_BASE_URL;
